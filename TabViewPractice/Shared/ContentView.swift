a6494c5224aeaafc8072347f8a9efbd1f29026ef
Youth <99013115+kimscastle@users.noreply.github.com>
2022-05-03 21:51:29 +0900

    Delete: .DS_Store

--- TabViewPractice/Shared/ContentView.swift
+++ TabViewPractice/Shared/ContentView.swift
@@ -5,33 +5,65 @@
 //  Created by uiskim on 2022/05/03.
 //
 
+//import SwiftUI
+//
+//struct Layout: View {
+//    @State private var selection = 0
+//    var body: some View {
+//        TabView(selection: $selection) {
+//            Text("The First Tab")
+//                .tabItem {
+//                    Image(systemName: "1.square.fill")
+//                    Text("First")
+//                }.tag(0)
+//            Text("Another Tab")
+//                .tabItem {
+//                    Image(systemName: "2.square.fill")
+//                    Text("Second")
+//                }.tag(1)
+//            Text("The Last Tab")
+//                .tabItem {
+//                    Image(systemName: "3.square.fill")
+//                    Text("Third")
+//                }.tag(2)
+//        }
+//        .font(.headline)
+//    }
+//}
+//
+//struct Layout_Previews: PreviewProvider {
+//    static var previews: some View {
+//            Layout()
+//    }
+//}
+
 import SwiftUI
 
 struct Layout: View {
+    @State private var selection = 0
     var body: some View {
-        TabView {
-            Text("The First Tab")
-                .tabItem {
-                    Image(systemName: "1.square.fill")
-                    Text("First")
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selection) {
+                Rectangle().foregroundColor(.red)
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .tag(0)
+                Rectangle().foregroundColor(.blue).tag(1)
+                Rectangle().foregroundColor(.green).tag(2)
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            .font(.headline)
+            
+            Button("next") {
+                withAnimation {
+                    if (selection > 2) {
+                        selection = 0
+                    } else {
+                        selection += 1
+                    }
                 }
-            Text("Another Tab")
-                .tabItem {
-                    Image(systemName: "2.square.fill")
-                    Text("Second")
-                }
-            ThirdView()
-                .tabItem {
-                    Image(systemName: "3.square.fill")
-                    Text("Third")
-                }
+            }
         }
-        .font(.headline)
     }
-}
-
-TabView{
-    
 }
 
 struct Layout_Previews: PreviewProvider {
