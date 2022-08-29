7842668f8312a54ecfc95131e9420961a376f992
kimscastle <uisong26559287@gmail.com>
2022-08-29 20:53:32 +0900

    :sparkles: 버튼누르면 리스트에 저장되는 기능

--- Nano_Challenge_2/Nano_Challenge_2/ViewController.swift
+++ Nano_Challenge_2/Nano_Challenge_2/ViewController.swift
@@ -8,12 +8,81 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let myQusetionList: [String] = [
+        "나는 다른사람이 강하게 맞다고 주장하면 납득하는 편이다",
+        "나는 주말엔 집에서 있는걸 선호하는 편이다",
+        "모르는게있으면 다른사람에게 물어보는 편이다",
+        "어색한 침묵이 흐를때 나는 먼저 말을 꺼내는 편이다",
+        "하나를 끝낸후에야 비로소 다른걸 할수 있는 편이다",
+        "오래전의 실수를 후회할때가 많다",
+        "휴식을 취하기전에 집안일을 먼저 끝내놓는 편이다"
+    ]
+    
+    var questionIndex = 0
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet var surveyResponseButtons: [UIButton]!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var chosedButton: Int?
+    var chosedButtonList: [Int] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        surveyResponseButtons.forEach { button in
+            view.addSubview(button)
+            makeNonActiveButton(button)
+        }
+        questionLabel.text = myQusetionList[0]
     }
+    
+    @IBAction func responseButtons(_ sender: UIButton) {
+        chosedButton = sender.tag
+        makeActiveButton(sender)
+        surveyResponseButtons.forEach { button in
+            if sender != button {
+                makeNonActiveButton(button)
+            }
+        }
+        
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        guard let chosedButton = chosedButton else { return print("버튼을 눌러주세요") }
+        chosedButtonList.append(chosedButton)
+        print(chosedButtonList)
+        surveyResponseButtons.forEach { makeNonActiveButton($0) }
+        self.chosedButton = nil
+    
+        questionIndex += 1
+        questionLabel.text = myQusetionList[questionIndex]
+        
+        
+        
+    }
+    
+}
 
-
+extension ViewController {
+    func makeNonActiveButton(_ button: UIButton) {
+        let config = UIImage.SymbolConfiguration(pointSize: 25, weight: .medium, scale: .default)
+        let nonActImage = UIImage(systemName: "circle.fill", withConfiguration: config)
+        button.layer.cornerRadius = 25
+        button.layer.borderWidth = 3
+        button.layer.borderColor = UIColor.systemGray2.cgColor
+        button.setImage(nonActImage, for: .normal)
+        button.tintColor = UIColor.systemGray2
+        button.setTitleColor(UIColor.systemGray2, for: .normal)
+    }
+    
+    func makeActiveButton(_ button: UIButton) {
+        let config = UIImage.SymbolConfiguration(pointSize: 25, weight: .medium, scale: .default)
+        let actImage = UIImage(systemName: "checkmark.circle.fill", withConfiguration: config)
+        button.layer.borderColor = UIColor.systemBlue.cgColor
+        button.setImage(actImage, for: .normal)
+        button.tintColor = UIColor.systemBlue
+        button.setTitleColor(.black, for: .normal)
+    }
 }
 
