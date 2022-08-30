30823fc710b8615bc1d4e987854b1ebf8c933906
kimscastle <uisong26559287@gmail.com>
2022-08-30 18:37:41 +0900

    :bug: 숫자및 범위 변경

--- Nano_Challenge_2/Nano_Challenge_2/SecondViewController.swift
+++ Nano_Challenge_2/Nano_Challenge_2/SecondViewController.swift
@@ -31,7 +31,7 @@ class SecondViewController: UIViewController {
         similarityLabel.text = makePercentage(totalResult[0].value) + "%"
         pagukNameLabel.text = totalResult[totalResult.count - 1].key
         pagukSimilarityLabel.text = makePercentage(totalResult[totalResult.count - 1].value) + "%"
-        switch totalResult[0].value {
+        switch (totalResult[0].value) * 100 {
         case 0..<30 :
             goodMent.text = "많이는 아니지만 제법(?) 비슷해요"
         case 30..<50 :
@@ -44,10 +44,10 @@ class SecondViewController: UIViewController {
             "우리 맞기는 한걸까...?"
         }
         
-        switch totalResult[totalResult.count - 1].value {
-        case 0..<5 :
+        switch (totalResult[totalResult.count - 1].value) * 100 {
+        case 0..<3 :
             badMent.text = "전생에 원수 사이였어도 이거보단 나을듯;;"
-        case 5..<10 :
+        case 3..<10 :
             badMent.text = "워어~ 머리부터 발끝까지 ~ 다안맞음."
         case 10..<20 :
             badMent.text = "어쩌면 비슷한 부분은 있을지도...?"
